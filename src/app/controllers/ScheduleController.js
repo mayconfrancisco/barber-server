8d4ad89bbd3784eed8dafd05b344b4e20dc920e5
Maycon Francisco <mayconjf@gmail.com>
2019-09-26 11:55:35 -0300

    Add provider appointments

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,38 @@
+import { parseISO, startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+/**
+ * Provider Appointments
+ */
+class ScheduleController {
+  async index(req, resp) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return resp.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return resp.json(appointments);
+  }
+}
+
+export default new ScheduleController();
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
